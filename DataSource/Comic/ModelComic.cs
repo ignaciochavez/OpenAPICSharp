0c0ef8f81d83892608d6c841c56473da647614f1
ignaciochavez <ignacio.chavez4646@gmail.com>
2023-02-20 03:37:57 -0300

    Se agrega bd

---
+++ DataSource/Comic/ModelComic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSource.Comic
+{
+    public static class ModelComic
+    {
+        private static ComicEntities comicEntities;
+
+        public static ComicEntities ComicEntities
+        {
+            get
+            {
+                if (comicEntities == null)
+                {
+                    comicEntities = new ComicEntities();
+                }
+
+                return comicEntities;
+            }
+        }
+    }
+}
